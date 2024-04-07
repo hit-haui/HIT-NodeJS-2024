80aed8fde8706e13678697ec51408443d7416cc2
dngiang <dongocgiang.edu@gmail.com>
2024-04-07 20:32:02 +0700

    ✨ Add base mvc

--- server.js
+++ server.js
@@ -1,43 +1,18 @@
-const httpStatus = require('http-status');
-const express = require('express');
-const path = require('path');
 require('dotenv').config();
+const express = require('express');
+const httpStatus = require('http-status');
+
+const viewRoute = require('./routes/view.route');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  const users = [
-    {
-      id: 1,
-      name: 'John',
-      isLocked: false,
-    },
-    {
-      id: 2,
-      name: 'Mika',
-      isLocked: true,
-    },
-    {
-      id: 3,
-      name: 'Kenvin',
-      isLocked: false,
-    },
-  ];
-  res.render('pages/index', { users });
-});
-
-app.get('/auth/login', (req, res) => {
-  res.render('pages/login')
-});
-
-app.get('/auth/register', (req, res) => {
-  res.render('pages/register')
-});
+app.use('/auth', viewRoute);
 
 app.all('*', (req, res) => {
   res.status(httpStatus.NOT_FOUND).send({
