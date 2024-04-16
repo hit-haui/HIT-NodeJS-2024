8483928fb2fb21888be10e3271919d5fd914d8d2
dngiang <dongocgiang.edu@gmail.com>
2024-04-16 20:06:11 +0700

    ✨ Create API CRUD user with database file

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,46 +1,104 @@
+const httpStatus = require('http-status');
+
+const User = require('../models/user.model');
+
 const createUser = (req, res) => {
-  const { fullName, email, password } = req.body;
-  res.json({ fullName, email, password });
-}
+  const { fullname, email } = req.body;
+  const users = User.create({ fullname, email, isLocked: false });
+  res.json({
+    message: 'Tạo mới người dùng thành',
+    code: httpStatus.OK,
+    data: {
+      users,
+    },
+  });
+};
 
 const getUsers = (req, res) => {
-  const reqQuery = req.query;
+  const users = User.findAll();
   res.json({
-    users: [],
-    queries: reqQuery
+    message: 'Lấy thành công mảng người dùng',
+    code: httpStatus.OK,
+    data: {
+      users,
+    },
   });
-}
+};
 
 const getUserById = (req, res) => {
   const { userId } = req.params;
+
+  const user = User.findById(userId);
+  if (!user) {
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
+      code: httpStatus.NOT_FOUND,
+    });
+  }
+
   res.json({
-    message: `Lấy thông tin người dùng có Id ${userId} thành công`
+    message: `Lấy thông tin người dùng thành công`,
+    code: httpStatus.OK,
+    data: {
+      user,
+    },
   });
-}
+};
 
 const updateUserById = (req, res) => {
   const { userId } = req.params;
-  const { fullName, password } = req.body;
+  const { fullname } = req.body;
+  const users = User.updateById(userId, { fullname });
+  if (!users) {
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
+      code: httpStatus.NOT_FOUND,
+    });
+  }
   res.json({
-    message: `Cập nhật thông tin người dùng có Id ${userId} thành công`,
-    fullName,
-    password
+    message: `Cập nhật thông tin người dùng thành công`,
+    code: httpStatus.OK,
+    data: {
+      users,
+    },
   });
-}
+};
 
 const deleteUserById = (req, res) => {
   const { userId } = req.params;
+  const users = User.deleteById(userId);
+  if (!users) {
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
+      code: httpStatus.NOT_FOUND,
+    });
+  }
   res.json({
-    message: `Xoá người dùng có Id ${userId} thành công`
+    message: `Xoá người dùng thành công`,
+    code: httpStatus.OK,
+    data: {
+      users,
+    },
   });
-}
+};
 
 const lockUserById = (req, res) => {
   const { userId } = req.params;
+  const user = User.lockById(userId);
+  if (!user) {
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
+      code: httpStatus.NOT_FOUND,
+    });
+  }
   res.json({
-    message: `Khoá người dùng có Id ${userId} thành công`
+    message: user.isLocked ? 'Khoá người dùng thành công' : 'Mở khoá người dùng thành công',
+    code: httpStatus.OK,
+    data: {
+      user,
+    },
   });
-}
+};
 
 module.exports = {
   createUser,
@@ -48,5 +106,5 @@ module.exports = {
   getUserById,
   updateUserById,
   deleteUserById,
-  lockUserById
-}
+  lockUserById,
+};
