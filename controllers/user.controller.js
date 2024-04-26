2f8479e4917ca42b3aaff06b4315627f59c67548
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-04-26 23:44:39 +0700

    btvn-buoi5/Nguyen-Thi-Thu-Hang

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,26 +1,23 @@
-const httpStatus = require("http-status");
-const User = require("../models/user.model");
+const httpStatus = require('http-status');
+
+const User = require('../models/user.model');
 
 const createUser = (req, res) => {
   const { fullname, email } = req.body;
-
-  const users = User.create({ fullname, email, islocked: false });
+  const users = User.create({ fullname, email, isLocked: false });
   res.json({
-    message: 'tao moi nguoi dung thanh cong!',
+    message: 'Tạo mới người dùng thành',
     code: httpStatus.OK,
     data: {
       users,
-    }
+    },
   });
 };
 
 const getUsers = (req, res) => {
-
   const users = User.findAll();
-
-  const reqQuery = req.query;
   res.json({
-    message: "lay thanh cong mang nguoi dung",
+    message: 'Lấy thành công mảng người dùng',
     code: httpStatus.OK,
     data: {
       users,
@@ -30,18 +27,21 @@ const getUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   const { userId } = req.params;
-  const user = User.findById(userId);
 
+  const user = User.findById(userId);
   if (!user) {
     res.status(httpStatus.NOT_FOUND).json({
-      message: `khong tim thay nguoi dung co id la ${userId}`,
+      message: `Không tìm thấy người dùng`,
       code: httpStatus.NOT_FOUND,
-    })
+    });
   }
+
   res.json({
     message: `Lấy thông tin người dùng thành công`,
     code: httpStatus.OK,
-    data: user,
+    data: {
+      user,
+    },
   });
 };
 
@@ -49,58 +49,54 @@ const updateUserById = (req, res) => {
   const { userId } = req.params;
   const { fullname } = req.body;
   const users = User.updateById(userId, { fullname });
-
   if (!users) {
-    return res.status(httpStatus.NOT_FOUND).json({
-      message: `khong tim thay nguoi dung co id la ${userId}`,
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
       code: httpStatus.NOT_FOUND,
-    })
+    });
   }
-
   res.json({
-    message: `Cập nhật thông tin người dùng có Id ${userId} thành công`,
+    message: `Cập nhật thông tin người dùng thành công`,
     code: httpStatus.OK,
-    data: users,
+    data: {
+      users,
+    },
   });
 };
 
 const deleteUserById = (req, res) => {
   const { userId } = req.params;
   const users = User.deleteById(userId);
-
   if (!users) {
-    return res.status(httpStatus.NOT_FOUND).json({
-      message: `khong tim thay nguoi dung co id la ${userId}`,
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
       code: httpStatus.NOT_FOUND,
-    })
+    });
   }
   res.json({
-    message: `Xoá người dùng  thành công`,
+    message: `Xoá người dùng thành công`,
     code: httpStatus.OK,
     data: {
-      users
-    }
+      users,
+    },
   });
 };
 
 const lockUserById = (req, res) => {
   const { userId } = req.params;
-
-  const user = User.lockId(userId);
-
-
+  const user = User.lockById(userId);
   if (!user) {
-    return res.status(httpStatus.NOT_FOUND).json({
-      message: `khong tim thay nguoi dung co id la ${userId}`,
+    res.status(httpStatus.NOT_FOUND).json({
+      message: `Không tìm thấy người dùng`,
       code: httpStatus.NOT_FOUND,
-    })
+    });
   }
   res.json({
-    message: user.islocked ? `Khoá người dùng thành công` : `Mo Khoá người dùng thành công`,
+    message: user.isLocked ? 'Khoá người dùng thành công' : 'Mở khoá người dùng thành công',
     code: httpStatus.OK,
     data: {
       user,
-    }
+    },
   });
 };
 
