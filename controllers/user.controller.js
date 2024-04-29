f613a6ec97dd76d699c9ac572a33660e6447c03f
Tung <ductungltt@gmail.com>
2024-04-29 17:11:40 +0700

    👍 Implement API delete user by id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -121,22 +121,40 @@ const updateUserById = async (req, res) => {
   }
 };
 
-const deleteUserById = (req, res) => {
+const deleteUserById = async (req, res) => {
   const { userId } = req.params;
-  const users = User.deleteById(userId);
-  if (!users) {
-    res.status(httpStatus.NOT_FOUND).json({
-      message: `Không tìm thấy người dùng`,
-      code: httpStatus.NOT_FOUND,
+
+  if (!/^[0-9a-fA-F]{24}$/.test(userId)) {
+    return res.status(httpStatus.BAD_REQUEST).json({
+      message: 'Vui lòng truyền đúng định dạng ObjectId',
+      code: httpStatus.BAD_REQUEST,
     });
   }
-  res.json({
-    message: `Xoá người dùng thành công`,
-    code: httpStatus.OK,
-    data: {
-      users,
-    },
-  });
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: `Không tìm thấy người dùng`,
+        code: httpStatus.NOT_FOUND,
+      });
+    }
+
+    res.json({
+      message: `Xoá người dùng thành công`,
+      code: httpStatus.OK,
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: 'Đã sảy ra lỗi vui lòng thử lại',
+      code: httpStatus.INTERNAL_SERVER_ERROR,
+    });
+  }
 };
 
 const lockUserById = (req, res) => {
