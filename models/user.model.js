9eefe8f007cf1e1c64764d9439bf2b0aa59ed37e
Tung <ductungltt@gmail.com>
2024-04-29 19:08:08 +0700

    ✨Hashing password for user

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema(
   {
@@ -34,6 +37,21 @@ const userSchema = new Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
+});
 
 const User = mongoose.model('User', userSchema);
 
