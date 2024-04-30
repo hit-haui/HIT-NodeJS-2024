23d4431d567e57e98cdbb56f863fa4e9947f6094
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-04-30 12:02:33 +0700

    btvn-buoi6/Nguyen-Thi-Thu-Hang

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -36,44 +35,4 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 
-=======
-
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema(
-  {
-    fullname: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    email: {
-      type: String,
-      trim: true,
-      unique: true,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    dateOfBirth: {
-      type: Date,
-      default: '2000-01-01',
-    },
-    isLocked: {
-      type: Boolean,
-      default: false,
-    },
-    avatar: {
-      type: String,
-      default: 'https://th.bing.com/th/id/OIP.z3fa8PjEnvzg4bhW61tEOwAAAA?rs=1&pid=ImgDetMain',
-    },
-  },
-  { timestamps: true },
-);
-
-const User = mongoose.model('User', userSchema);
-
->>>>>>> cce3895b839e12105af31622b5ab626629b0d96a
 module.exports = User;
