c37efcf140b29ba64fc44793bed93d5dc104a39d
dngiang <dongocgiang.edu@gmail.com>
2024-05-04 19:20:21 +0700

    ✨ Create model class

---
+++ models/class.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const classSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    numberOfCredits: {
+      type: Number,
+      default: 3,
+      required: true,
+    },
+    maxStudentQuantity: {
+      type: Number,
+      default: 65,
+      required: true,
+    },
+    place: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      default: '2000-01-01',
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      default: '2000-01-01',
+      required: true,
+    },
+    teacher: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const Class = mongoose.model('Class', classSchema);
+
+module.exports = Class;
