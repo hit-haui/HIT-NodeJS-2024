4469422834b82d908210fb668dff0418b016da4c
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-05-14 23:05:10 +0700

    ✨ custom apiError

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,14 @@
+const httpStatus = require("http-status");
+
+
+const errorHandler = (error, req, res, next) => {
+    const code = error.status || httpStatus.INTERNAL_SERVER_ERROR;
+    const message = error.message || "internal server!";
+
+    res.status(code).json({
+        message,
+        code,
+    });
+};
+
+module.exports = errorHandler;
