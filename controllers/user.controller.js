8e05e59027f06ab0ab62f88ff58bba65f8caf299
Tranggg1210 <nguyenthitrang.ttd@gmail.com>
2024-05-25 08:10:54 +0700

    ✨ complete update APIs from try - catch to catchAsync.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,7 +5,7 @@ const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const checkIdMongo = require('../utils/check-id-mongo');
 
-const createUser = catchAsync(async (req, res, next) => {
+const createUser = catchAsync(async (req, res) => {
   const { fullname, email, password } = req.body;
 
   if (!fullname || !email || !password) {
@@ -27,65 +27,54 @@ const createUser = catchAsync(async (req, res, next) => {
   });
 });
 
-const getUsers = async (req, res, next) => {
+const getUsers = catchAsync(async (req, res) => {
   const { limit = 10, page = 1, sortBy = 'createdAt : desc, fullname: desc' } = req.query;
 
   const skip = (+page - 1) * +limit;
 
   const [field, value] = sortBy.split(':');
   const sort = { [field]: value === 'asc' ? 1 : -1 };
+  const query = {};
 
-  try {
-    const query = {};
+  const users = await User.find().limit(limit).skip(skip).sort(sort);
 
-    const users = await User.find().limit(limit).skip(skip).sort(sort);
+  const totalResults = await User.countDocuments(query);
 
-    const totalResults = await User.countDocuments(query);
+  res.json({
+    message: 'Lấy thành công mảng người dùng',
+    code: httpStatus.OK,
+    data: {
+      users,
+      limit: +limit,
+      currentPage: +page,
+      totalPage: Math.ceil(totalResults / +limit),
+      totalResults,
+    },
+  });
+})
 
-    res.json({
-      message: 'Lấy thành công mảng người dùng',
-      code: httpStatus.OK,
-      data: {
-        users,
-        limit: +limit,
-        currentPage: +page,
-        totalPage: Math.ceil(totalResults / +limit),
-        totalResults,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};
-
-const getUserById = async (req, res, next) => {
+const getUserById = catchAsync(async (req, res) => {
   const { userId } = req.params;
 
   if (!checkIdMongo(userId)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Vui lòng truyền đúng định dạng ObjectId');
   }
 
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
-    }
-
-    res.json({
-      message: `Lấy thông tin người dùng thành công`,
-      code: httpStatus.OK,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
   }
-};
 
-const updateUserById = async (req, res, next) => {
+  res.json({
+    message: `Lấy thông tin người dùng thành công`,
+    code: httpStatus.OK,
+    data: {
+      user,
+    },
+  });
+})
+
+const updateUserById = catchAsync(async (req, res) => {
   const { userId } = req.params;
   const updateBody = req.body;
 
@@ -93,86 +82,69 @@ const updateUserById = async (req, res, next) => {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Vui lòng truyền đúng định dạng ObjectId');
   }
 
-  try {
-    const user = await User.findById(userId);
+  const user = await User.findById(userId);
 
-    if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
-    }
-
-    Object.assign(user, updateBody);
-
-    await user.save();
-
-    res.json({
-      message: `Cập nhật thông tin người dùng thành công`,
-      code: httpStatus.OK,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
   }
-};
 
-const deleteUserById = async (req, res, next) => {
+  Object.assign(user, updateBody);
+
+  await user.save();
+
+  res.json({
+    message: `Cập nhật thông tin người dùng thành công`,
+    code: httpStatus.OK,
+    data: {
+      user,
+    },
+  });
+})
+
+const deleteUserById = catchAsync(async (req, res) => {
   const { userId } = req.params;
 
   if (!checkIdMongo(userId)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Vui lòng truyền đúng định dạng ObjectId');
   }
+  const user = await User.findByIdAndDelete(userId);
 
-  try {
-    const user = await User.findByIdAndDelete(userId);
-
-    if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
-    }
-
-    res.json({
-      message: `Xoá người dùng thành công`,
-      code: httpStatus.OK,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
   }
-};
 
-const lockUserById = async (req, res, next) => {
+  res.json({
+    message: `Xoá người dùng thành công`,
+    code: httpStatus.OK,
+    data: {
+      user,
+    },
+  });
+})
+
+const lockUserById = catchAsync(async (req, res) => {
   const { userId } = req.params;
 
   if (!checkIdMongo(userId)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Vui lòng truyền đúng định dạng ObjectId');
   }
+  const user = await User.findById(userId);
 
-  try {
-    const user = await User.findById(userId);
-
-    if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
-    }
-
-    user.isLocked = !user.isLocked;
-    await user.save();
-
-    res.json({
-      message: user.isLocked ? 'Khoá người dùng thành công' : 'Mở khoá người dùng thành công',
-      code: httpStatus.OK,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, `Không tìm thấy người dùng`);
   }
-};
+
+  user.isLocked = !user.isLocked;
+  await user.save();
+
+  res.json({
+    message: user.isLocked ? 'Khoá người dùng thành công' : 'Mở khoá người dùng thành công',
+    code: httpStatus.OK,
+    data: {
+      user,
+    },
+  });
+})
 
 module.exports = {
   createUser,
